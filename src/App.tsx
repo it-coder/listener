035585839a1d1581cff50688206fc32214e07d93
alistar <codingbuilder@gmail.com>
2023-05-22 17:47:17 +0800

    dinit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style/common.css'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="main">
+        <div className="sidebar"></div>
+        <div className="content"></div>
+      </div>
+      <div className="footer">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
