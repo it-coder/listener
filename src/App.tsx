57c767c83859233376d8c65ca9cf7e9f29081266
alistar <codingbuilder@gmail.com>
2023-05-22 18:50:09 +0800

    test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './style/common.css'
+import { invoke } from '@tauri-apps/api/tauri';
 
 function App() {
+  function cl() {
+    invoke('greet', {name:"world"}).then((resp) => console.log(resp))
+  }
   return (
     <div className="wrapper">
       <div className="main">
@@ -9,7 +13,7 @@ function App() {
         <div className="content"></div>
       </div>
       <div className="footer">
-
+        <button onClick={cl} >点我</button>
       </div>
     </div>
   );
