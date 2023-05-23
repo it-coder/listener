13515eb8c7b2f806538c9c12455972e17e60ff43
alistar <codingbuilder@gmail.com>
2023-05-23 18:56:01 +0800

    test netease

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,11 +13,11 @@ fn main() {
     //     .run(tauri::generate_context!())
     //     .expect("error while running tauri application");
 
-    neteaseTest();
+    netease_test();
 }
 
 use scraper::{Html, Selector};
-fn neteaseTest() {
+fn netease_test() {
     let uri = "https://music.163.com/discover/playlist/".to_string();
     let resp = reqwest::blocking::get(uri).unwrap_or_else(| err | {
         panic!("show_playlist error is {}", err);
@@ -25,36 +25,56 @@ fn neteaseTest() {
     let text = resp.text().unwrap();
 
     let fragment = Html::parse_fragment(&text);
-    let liSelector = Selector::parse("ul.m-cvrlst li").unwrap();
+    let li_selector = Selector::parse("ul.m-cvrlst li").unwrap();
 
 
-    let imageSelector = Selector::parse("div.u-cover img").unwrap();
+    let image_selector = Selector::parse("div.u-cover img").unwrap();
 
-    let detailSelector = Selector::parse("div.u-cover a.msk").unwrap();
+    let detail_selector = Selector::parse("div.u-cover a.msk").unwrap();
 
 
-    for liElement in fragment.select(&liSelector) {
-        let imageElement = liElement.select(&imageSelector);
-        let image = imageElement.last().unwrap();
+    for li_element in fragment.select(&li_selector) {
+        let image_element = li_element.select(&image_selector);
+        let image = image_element.last().unwrap();
         println!("{:?}", image.value().attr("src").unwrap());
-        // imageElement.for_each(|x|  {
-        //     println!("{:?}", x.value().attr("src").unwrap());
+       
 
-        // });
+        let detail_element = li_element.select(&detail_selector);
 
-        let detailElement = liElement.select(&detailSelector);
-
-        let detail = detailElement.last().unwrap();
+        let detail = detail_element.last().unwrap();
         println!("{:?}", detail.value().attr("href").unwrap());
         println!("{:?}", detail.value().attr("title").unwrap());
-
-        // detailElement.for_each(|x| {
-        //     println!("{:?}", x.value().attr("href").unwrap());
-        //     println!("{:?}", x.value().attr("title").unwrap());
-
-        // });
-
+        let cover_img_url = image.value().attr("src").unwrap().replace("140y140", "512y512");
+        let title = detail.value().attr("title").unwrap().to_string();
+        let id = detail.value().attr("href").unwrap().to_string().split("=").collect::<Vec<&str>>()[1].to_string();
+        
+        let mut source_url = String::from("https://music.163.com/#/playlist?id=");
+        source_url.push_str(&id);
+        let mut id_u8 = String::from("neplaylist_");
+        id_u8.push_str(&id);
+        
+        let song = SongList::new(cover_img_url, id_u8, source_url, title);
+        println!("song is {:#?}", song);
     }
 
     
+}
+
+#[derive(Debug)]
+struct SongList {
+    cover_img_url: String,
+    id: String,
+    source_url: String,
+    title: String,
+}
+
+impl SongList {
+    fn new(cover_img_url: String, id: String, source_url: String, title: String) -> Self {
+        SongList {
+            cover_img_url,
+            id,
+            source_url,
+            title,
+        }
+    }
 }
