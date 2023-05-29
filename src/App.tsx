be6c0962e006059c6b56c029813220015802dc9b
alistar <codingbuilder@gmail.com>
2023-05-29 22:59:51 +0800

    css from listen1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import './style/common.css'
 import  { Netease } from './provider/netease'
 
-import client from './server/index'
+import './static/css/common.css'
+import './static/css/icon.css'
+import './static/css/iparanoid.css'
+// import './style/common.css'
 
   
 
@@ -11,11 +13,7 @@ function App() {
 
   async function test_http() {
     
-    client.get("https://baidu.com").then((resp) => {
-      console.log(resp);
-    }).catch((err) => {
-      console.log(err);
-    })
+    
   }
 
   function test_netease() {
@@ -25,22 +23,25 @@ function App() {
     <div className="wrapper">
       <div className="main">
         <div className="sidebar">
-          <div className="flex-scroll-wrapper"></div>
-          <div className="menu-control"></div>
-          <div className="menu-title">
-            <div className="title">平台聚合</div>
+          <div className="flex-scroll-wrapper">
+            <div className="menu-control"></div>
+            <div className="menu-title">
+              <div className="title">平台聚合</div>
+            </div>
+            <ul className="nav masthead-nav">
+              <li>
+                <div className="sidebar-block">
+                  <span className="icon li-featured-list"></span>
+                  <a>精选歌单</a>
+                </div>
+              </li>
+            </ul>
           </div>
-          <ul className="nav masthead-nav">
-            <li>
-              <div className="sidebar-block">
-                <span className="icon li-featured-list"></span>
-                <a>精选歌单</a>
-              </div>
-            </li>
-          </ul>
+        </div>
+        <div className="content">
+          
 
         </div>
-        <div className="content"></div>
       </div>
       <div className="footer">
         <button onClick={test_http} >点我</button>
