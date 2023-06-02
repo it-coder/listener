746f637b5c8ae8dd7f31863eaeedef017bd53fc7
alistar <codingbuilder@gmail.com>
2023-06-02 22:58:14 +0800

    platform components update

--- src/components/Platform.tsx
+++ src/components/Platform.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-interface PlatformProps {
-    sourceList: Array<Source>
+interface IProps {
+    sourceList?: Array<Source>
+    source?: Source
 };
 
 interface Source {
@@ -12,25 +13,33 @@ interface Source {
 
  
 
-const Platform = (props: PlatformProps) => {
+const Platform = (props: IProps) => {
     const { sourceList } = props;
     return (
         <div className="source-list" ng-show="is_window_hidden==1">
             {
-                sourceList.map((source, index) => {
+                sourceList?.map((source, index) => {
                     return (
-                        <>
-                            <div className="source-button">
-                                {source.displayText}
-                            </div>
-                            <div className="splitter"></div>
-                        </>
+                        <PlatformItem source={source} key={source.displayId} />
                     );
                 })
             }
         </div>
     );
 };
+
+
+const PlatformItem = (props: IProps) => {
+    const {source} = props;
+    return (
+        <>
+            <div className="source-button" >
+                {source?.displayText}
+            </div>
+            <div className="splitter"></div>
+        </>
+    );
+}
 
 
 export default Platform;
