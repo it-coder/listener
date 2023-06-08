da7f4aa95f1f8094e90856b5880c1782fba71855
alistar <codingbuilder@gmail.com>
2023-06-08 21:36:43 +0800

    fix merge

--- src/App.tsx
+++ src/App.tsx
@@ -64,17 +64,7 @@ function App() {
   let filterId = ""
 
 
-  // 切换平台
-  const onTogglePlatform: TogglePlatform = (id: string) => {
-    channelId = id;
-    filterRef.current?.changeChannel(channelId)
-  }
- 
-  // 筛选歌曲分类
-  const onToggleFilter: ToggleFilter = (id:string) => {
-    filterId = id
-    playlistRef.current?.init(channelId, filterId)
-  }
+  
 
 
   return (
